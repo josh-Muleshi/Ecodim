4dfa4b81b66edcdffea16fca6c4c590e8c7ae5ba
josh muleshi <jmuleshi2@gmail.com>
2020-12-20 21:58:36 +0000

    quelques derniers ajustements sur la bottom bar

--- app/src/main/java/cd/amateurmobiledev/ecodim/HomeActivity.java
+++ app/src/main/java/cd/amateurmobiledev/ecodim/HomeActivity.java
@@ -29,7 +29,9 @@ public class HomeActivity extends AppCompatActivity {
 
         navigationView.initWithSaveInstanceState(savedInstanceState);
         navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_home_black_24));
-        navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_add_black_24));
+        navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_search_black_24));
+        navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_baseline_notifications_24));
+        navigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_baseline_account_circle_24));
 
         navigationView.setSpaceOnClickListener(new SpaceOnClickListener() {
             @Override
