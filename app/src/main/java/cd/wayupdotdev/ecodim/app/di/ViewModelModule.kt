50201068d5c100c6d0c20118ff0c1ede1fa15b8b
Josh Muleshi <jmuleshi2@gmail.com>
2025-03-30 15:07:19 -0400

    viewmodel module done

---
+++ app/src/main/java/cd/wayupdotdev/ecodim/app/di/ViewModelModule.kt
@@ -0,0 +1,13 @@
+package cd.wayupdotdev.ecodim.app.di
+
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val viewModelModule = module {
+//    viewModelOf(::AuthViewModel)
+//    viewModelOf(::HomeViewModel)
+//    viewModelOf(::SignInViewModel)
+//    viewModelOf(::SettingViewModel)
+//    viewModelOf(::ProfileViewModel)
+//    viewModelOf(::ProductDetailViewModel)
+}
