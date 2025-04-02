1762862044186a6dc413464ad4552157bc55ab8d
Josh Muleshi <jmuleshi2@gmail.com>
2025-04-02 14:44:49 -0400

    topic well shown

--- app/src/main/java/cd/wayupdotdev/ecodim/features/topicDetail/TopicDetailScreen.kt
+++ app/src/main/java/cd/wayupdotdev/ecodim/features/topicDetail/TopicDetailScreen.kt
@@ -1,8 +1,12 @@
 package cd.wayupdotdev.ecodim.features.topicDetail
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.outlined.FavoriteBorder
@@ -15,8 +19,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -39,12 +46,21 @@ fun TopicDetailScreen(
         viewModel.getTopicDetail(topicUid)
     }
 
+    val scale = remember { mutableFloatStateOf(1f) }
+    val offsetX = remember { mutableFloatStateOf(0f) }
+    val offsetY = remember { mutableFloatStateOf(0f) }
+
+    val state = rememberTransformableState { zoomChange, offsetChange, _ ->
+        scale.value *= zoomChange
+        offsetX.value += offsetChange.x
+        offsetY.value += offsetChange.y
+    }
+
     Scaffold(
         modifier = modifier,
         topBar = {
             TopAppBar(
-                title = {
-                },
+                title = { },
                 navigationIcon = {
                     IconButton(onClick = onBackBtnClicked) {
                         Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Retour")
@@ -63,13 +79,21 @@ fun TopicDetailScreen(
     ) { paddingValues ->
         if (data is TopicDetailUiState.Success) {
             val lesson = (data as TopicDetailUiState.Success).lesson
-            Column(
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
+                    .verticalScroll(rememberScrollState())
+                    .transformable(state = state)
+                    .graphicsLayer(
+                        scaleX = scale.floatValue,
+                        scaleY = scale.floatValue,
+                        translationX = offsetX.floatValue,
+                        translationY = offsetY.floatValue
+                    )
             ) {
                 MarkdownText(
-                    modifier = Modifier.padding(8.dp),
+                    modifier = Modifier.padding(horizontal = 16.dp),
                     markdown = lesson.content,
                     style = TextStyle(
                         color = Color.Blue,
@@ -82,3 +106,4 @@ fun TopicDetailScreen(
         }
     }
 }
+
