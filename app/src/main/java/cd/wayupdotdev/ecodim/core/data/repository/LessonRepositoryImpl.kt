ad7d395c9b6550803a2033b3f3189f4fb04c46b6
Josh Muleshi <jmuleshi2@gmail.com>
2025-04-07 03:21:48 -0400

    refactoring

--- app/src/main/java/cd/wayupdotdev/ecodim/core/data/repository/LessonRepositoryImpl.kt
+++ app/src/main/java/cd/wayupdotdev/ecodim/core/data/repository/LessonRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package cd.wayupdotdev.ecodim.core.data.repository
 
-import android.util.Log
 import cd.wayupdotdev.ecodim.core.data.local.dao.LessonDao
 import cd.wayupdotdev.ecodim.core.data.local.entity.LessonEntity
 import cd.wayupdotdev.ecodim.core.data.remote.model.Lesson
@@ -16,77 +15,6 @@ import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
-
-//class LessonRepositoryImpl(
-//    private val firestore: FirebaseFirestore,
-//    private val lessonDao: LessonDao
-//) : LessonRepository {
-//
-//    override fun getAll(): Flow<List<Lesson>> = lessonDao.getAllLessons()
-//        .flatMapLatest { localLessons ->
-//            callbackFlow {
-//                val listener = firestore.collection(FireBaseConstants.lesson)
-//                    .orderBy(Lesson::createdAt.name, Query.Direction.DESCENDING)
-//                    .addSnapshotListener { value, error ->
-//                        if (error != null) {
-//                            close(error)
-//                            return@addSnapshotListener
-//                        }
-//
-//                        val lessons = value?.toObjects(Lesson::class.java).orEmpty()
-//
-//                        launch(Dispatchers.IO) {
-//                            updateLocalDatabase(lessons)
-//                        }
-//
-//                        trySend(lessons).isSuccess
-//                    }
-//
-//                awaitClose { listener.remove() }
-//            }
-//        }.flowOn(Dispatchers.IO)
-//    private suspend fun updateLocalDatabase(lessons: List<Lesson>) {
-//        val entities = lessons.mapNotNull { lesson ->
-//            lesson.createdAt?.let {
-//                LessonEntity(
-//                    id = lesson.uid,
-//                    userId = lesson.userId,
-//                    content = lesson.content,
-//                    updatedAt = it.time
-//                )
-//            }
-//        }
-//        lessonDao.insertLessons(entities)
-//    }
-//
-//    override fun getLessonByUid(uidLesson: String): Flow<LessonEntity?> =
-//        lessonDao.getLessonByUid(uidLesson)
-//
-//    override fun getFavoriteLessons(): Flow<List<LessonEntity>> =
-//        lessonDao.getFavoriteLessons()
-//
-////    override suspend fun updateFavorite(lessonId: String, isFavorite: Boolean) {
-////        lessonDao.updateFavorite(lessonId, isFavorite)
-////    }
-//
-//    override suspend fun updateLocalDatabase(lessons: List<Lesson>) {
-//        val updatedEntities = lessons.mapNotNull { lesson ->
-//            lesson.createdAt?.let {
-//                val localFavorite = lessonDao.getLessonByUid(lesson.uid).firstOrNull()?.isFavorite ?: false
-//                LessonEntity(
-//                    id = lesson.uid,
-//                    userId = lesson.userId,
-//                    content = lesson.content,
-//                    updatedAt = it.time,
-//                    isFavorite = localFavorite // On conserve la valeur locale
-//                )
-//            }
-//        }
-//        lessonDao.insertLessons(updatedEntities)
-//    }
-//
-//
-//}
 
 class LessonRepositoryImpl(
     private val firestore: FirebaseFirestore,
